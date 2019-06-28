c657c963a6cdccfa8f56f8ac6f5f6759682f0e72
Denis, Makarov <makarovdenis07081985@gmail.com>
2019-06-28 17:30:18 +0300

    work with line

--- src/js/content.js
+++ src/js/content.js
@@ -1,35 +1,64 @@
 let slides = Array.from(document.getElementsByClassName("change_line"))
 let btnChangeSlide = document.getElementById("btn_chahge");
 
-let classAdd;
+// let classAdd;
 let current = 1;
-let x=0;
+let bgPosition = 0;
 
 let arrLine = Array.from(document.getElementsByClassName("change_line"));
+let arrLineFirstBg = Array.from(document.getElementsByClassName("change_line_bg"));
 let arrLineFirstBg1 = Array.from(document.getElementsByClassName("change_line_bg_2"));
 let arrLineFirstBg2 = Array.from(document.getElementsByClassName("change_line_bg_1"));
 let arrLineFirstBg3 = Array.from(document.getElementsByClassName("change_line_bg_3"));
 let change_line_cover = Array.from(document.getElementsByClassName("change_line_cover"));
 
 
-
-for(i=0; i < arrLineFirstBg1.length; i++){
-    arrLineFirstBg1[i].style.backgroundPositionX = x + "%";
-    arrLineFirstBg2[i].style.backgroundPositionX = x + "%";
-    arrLineFirstBg3[i].style.backgroundPositionX = x + "%";
-    x=x+5;
+for (i = 0; i < arrLineFirstBg1.length; i++) {
+    arrLineFirstBg1[i].style.backgroundPositionX = bgPosition + "%";
+    arrLineFirstBg2[i].style.backgroundPositionX = bgPosition + "%";
+    arrLineFirstBg3[i].style.backgroundPositionX = bgPosition + "%";
+    bgPosition = bgPosition + 5;
 }
 
-for(i=0; i < change_line_cover.length; i++){
+for (i = 0; i < change_line_cover.length; i++) {
     change_line_cover[i].setAttribute("data-name", i);
-    
-    change_line_cover[i].addEventListener("click", (e)=>{
-        // console.log(e.target.dataset.name)
-        chngeOpacity()
 
-        console.log(current)
-    })
+    change_line_cover[i].addEventListener("click", (e) => {
+
+            let numTargetLineRight = +e.target.dataset.name;
+            let numTargetLineLeft = +e.target.dataset.name;
+            let x=1
+            let y=1
+
+
+            for (j = numTargetLineRight; j < change_line_cover.length; j++) {
+
+                arrLineFirstBg1[j].style.transitionDelay = "0s";
+                arrLineFirstBg2[j].style.transitionDelay = "0s";
+                arrLineFirstBg3[j].style.transitionDelay = "0s";
+
+                arrLineFirstBg1[j].style.transitionDelay = x / 25 + "s";
+                arrLineFirstBg2[j].style.transitionDelay = x / 25 + "s";
+                arrLineFirstBg3[j].style.transitionDelay = x / 25 + "s";
+                numTargetLineRight = numTargetLineRight + 1;
+                x = x+1
+
+            }
+
+            for (l = numTargetLineLeft; l >= 0; l--) {
+                arrLineFirstBg1[l].style.transitionDelay = y / 25 + "s";
+                arrLineFirstBg2[l].style.transitionDelay = y / 25 + "s";
+                arrLineFirstBg3[l].style.transitionDelay = y / 25 + "s";
+                numTargetLineLeft = numTargetLineLeft + 1;
+                y = y+1
+            }
+
+            chngeOpacity();
+            // console.log(current)
+        }
+    )
 }
+
 
 function chngeOpacity() {
     if (current == 0) {
@@ -37,41 +66,18 @@ function chngeOpacity() {
         arrLineFirstBg2.map(el => el.style.opacity = "0");
         arrLineFirstBg1.map(el => el.style.opacity = "1");
         current = 1
-    }
-    else if (current == 1) {
+    } else if (current == 1) {
         arrLineFirstBg3.map(el => el.style.opacity = "0");
         arrLineFirstBg1.map(el => el.style.opacity = "0");
         arrLineFirstBg2.map(el => el.style.opacity = "1");
         current = 2
-    }
-    else if (current == 2) {
+    } else if (current == 2) {
         arrLineFirstBg2.map(el => el.style.opacity = "0");
         arrLineFirstBg1.map(el => el.style.opacity = "0");
         arrLineFirstBg3.map(el => el.style.opacity = "1");
         current = 0
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // function setBgPosition() {
@@ -132,14 +138,6 @@ function chngeOpacity() {
 //     slides[j].classList.add(classAdd);
 // }
 //
-function changeNum() {
-    if (current == 2) {
-        current = 1
-    } else if (current == 1) {
-        current = 3
-    } else if (current == 3) {
 
-        current = 2
-    }
-}
+
 //
